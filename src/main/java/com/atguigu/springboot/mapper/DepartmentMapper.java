132d30185430378619915d990f4a8b6dfceabfe0
ljh0718 <46011581+ljh0718@users.noreply.github.com>
2019-08-05 16:11:12 +0800

    注解版接口
    添加了一些注释

--- src/main/java/com/atguigu/springboot/mapper/DepartmentMapper.java
+++ src/main/java/com/atguigu/springboot/mapper/DepartmentMapper.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 /**
  * @author Mr.Z
  * @create 2019/8/5 14:19
+ * 此接口为MyBatis的注解版接口
  */
-//@Component
 //@Mapper
 public interface DepartmentMapper {
 
@@ -18,6 +18,7 @@ public interface DepartmentMapper {
     @Delete("delete from department where id=#{id}")
     public int deleteById(Integer id);
 
+    //获取自增主键
     @Options(useGeneratedKeys = true,keyProperty = "id")
     @Insert("insert into department(department_name) values(#{departmentName})")
     public int insertDep(Department department);
